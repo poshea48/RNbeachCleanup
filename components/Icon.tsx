4185d91d91b7ffa4a354135c0feffac69969e395
Paul O'Shea <poshea48@msn.com>
2020-10-09 07:24:19 -1000

    updated Icon to ts

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export default ({ name, ...props }) => (
+interface IconType {
+  name: string;
+}
+export default ({ name, ...props }: IconType) => (
   <Icon
     name={Platform.OS === 'ios' ? `ios-${name}` : `md-${name}`}
     {...props}
