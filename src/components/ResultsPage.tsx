16509bdf2902c5de422cf3894cd7e73443c5d7f6
Paul O'Shea <poshea48@msn.com>
2020-10-28 16:45:54 -1000

    Added results page display

--- src/components/ResultsPage.tsx
+++ src/components/ResultsPage.tsx
@@ -1,21 +1,66 @@
+/* eslint-disable react-native/no-raw-text */
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { DataTable, Headline } from 'react-native-paper';
+import { useAppState } from '../context/appContext';
 import colors from '../../colors';
 
 const ResultsPage: React.FC = () => {
+  const { debris } = useAppState();
+  let displayRows;
+
+  if (debris) {
+    displayRows = Object.keys(debris).map((item, i) => (
+      <DataTable.Row key={`${item}-${i}`}>
+        <DataTable.Cell>{item}</DataTable.Cell>
+        <DataTable.Cell>{debris[item]}</DataTable.Cell>
+      </DataTable.Row>
+    ));
+  }
   return (
     <View style={styles.container}>
-      <Text>THis will be the results page</Text>
+      <Headline style={styles.headline}>Final Results</Headline>
+      {debris ? (
+        <DataTable style={styles.dataTable}>
+          <DataTable.Header>
+            <DataTable.Title>
+              <Text>Item Collected</Text>
+            </DataTable.Title>
+            <DataTable.Title numeric>
+              <Text>Total</Text>
+            </DataTable.Title>
+          </DataTable.Header>
+          {displayRows}
+        </DataTable>
+      ) : (
+        <Headline>
+          <Text style={styles.noResults}>No results yet</Text>
+        </Headline>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10,
-    justifyContent: 'center',
+    paddingTop: 50,
+    justifyContent: 'flex-start',
     height: '100%',
-    backgroundColor: colors.main,
+    backgroundColor: colors.white,
+  },
+  headline: {
+    color: colors.main,
+    alignSelf: 'center',
+    fontWeight: '800',
+    textTransform: 'uppercase',
+  },
+  dataTable: {
+    width: '100%',
+  },
+  noResults: {
+    color: colors.orange,
+    alignSelf: 'center',
+    textAlign: 'center',
   },
   // heading: {},
   // button: {
